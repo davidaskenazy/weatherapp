9d3cf239981260649b1145d1b7364423bdc6d5e8
davidaskenazy <davidask.da@gmail.com>
2020-10-01 17:30:12 -0300

    app terminada, problema con yalm importacion

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,125 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
+//2cded736ac1666bfb34403a845a084e7
+//api.openweathermap.org/data/2.5/weather?q={Boston}&appid={2cded736ac1666bfb34403a845a084e7}
+void main() => runApp(MaterialApp(
+      title: "Weather App",
+      home: Home(),
+    ));
+
+class Home extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _HomeState();
+  }
+}
+
+class _HomeState extends State<Home> {
+  var temp;
+  var description;
+  var currently;
+  var humidity;
+  var windSpeed;
+
+  Future getWeather() async {
+    http.Response response = await http.get(
+        "http://api.openweathermap.org/data/2.5/weather?q=Boston&appid=2cded736ac1666bfb34403a845a084e7");
+    var results = jsonDecode(response.body);
+    print(results);
+    setState(() {
+      this.temp = results['main']['temp'];
+      this.description = results['wheather'][0]['description'];
+      this.currently = results['wheather'][0]['main'];
+      this.humidity = results['main']['humidity'];
+      this.windSpeed = results['wind']['speed'];
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    this.getWeather();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(children: <Widget>[
+        Container(
+            height: MediaQuery.of(context).size.height / 3,
+            width: MediaQuery.of(context).size.width,
+            color: Colors.red,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.only(bottom: 10.0),
+                  child: Text(
+                    "Currently in Boscon",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 14.0,
+                        fontWeight: FontWeight.w600),
+                  ),
+                ),
+                Text(
+                  temp != null ? temp.toString() + "\u0080" : "Loading",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 40.0,
+                      fontWeight: FontWeight.w600),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 10.0),
+                  child: Text(
+                    currently != null ? currently.toString() : "Loading",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 14.0,
+                        fontWeight: FontWeight.w600),
+                  ),
+                ),
+              ],
+            )),
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.all(20.0),
+            child: ListView(
+              //aca
+              children: <Widget>[
+                ListTile(
+                  leading: FaIcon(FontAwesomeIcons.thermometerHalf),
+                  title: Text("Temperature"),
+                  trailing: Text(
+                      temp != null ? temp.toString() + "\u0080" : "Loading"),
+                ),
+                ListTile(
+                  leading: FaIcon(FontAwesomeIcons.cloud),
+                  title: Text("Weather"),
+                  trailing: Text(
+                      description != null ? description.toString() : "Loading"),
+                ),
+                ListTile(
+                  leading: FaIcon(FontAwesomeIcons.sun),
+                  title: Text("Humidity"),
+                  trailing:
+                      Text(humidity != null ? humidity.toString() : "Loading"),
+                ),
+                ListTile(
+                  leading: FaIcon(FontAwesomeIcons.wind),
+                  title: Text("Wind Speed"),
+                  trailing: Text(
+                      windSpeed != null ? windSpeed.toString() : "Loading"),
+                )
+              ],
+            ),
+          ),
+        )
+      ]),
+    );
+  }
+}
